09d9de22065919972eebe28e7df77326484b527b
Strezex <124489820+Strezex@users.noreply.github.com>
2023-02-12 11:42:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function solution() {
+    let a = document.getElementById('a').value
+    let b = document.getElementById('b').value
+    document.getElementById('sum').innerText = +a + +b
+
+}
+function multiplication() {
+    let a = document.getElementById('a').value
+    let b = document.getElementById('b').value
+    document.getElementById('sum').innerText = +a * +b
+}
